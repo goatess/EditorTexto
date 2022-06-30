2efe4c4164f304243d0c8d96105df39d96975c89
goatess <107187023+goatess@users.noreply.github.com>
2022-06-30 12:19:48 +0200

    basic counter done

--- src/App.java
+++ src/App.java
@@ -1,15 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        SearchWord searchWord = new SearchWord();
+        searchWord.WordsCounter();
     }
 }
 
 class SearchWord{
-    private String search_word = "";
+    //new counter parameters
     private String text = "";
+    private String [] words = null;
+    //old functions parameters
+    private String search_word = "";
     private boolean isFound = false;
     private int word_counter = 0;
+    private int text_length = text.length();
     
-    
+    // New Counter
+    void WordsCounter(){
+        words = text.split(" ");
+        
+    }
+
+    // Old functions
     boolean search(){
         isFound = text.contains(search_word);
         word_counter();
@@ -17,37 +29,46 @@ class SearchWord{
     }
     
     int word_counter(){
-        for (int i = 0; i < text.length(); i++) {
-            if (text.substring(i).startsWith(search_word)) {
+        for (int index = 0; index < text.length(); index++) {
+            if (text.substring(index).startsWith(search_word)) {
                 word_counter ++;
             }
         }
-        return word_counter;
-        
+        return word_counter;      
     }
-
-public String getSearch_word() {
-    return search_word;
-}
-public String getText() {
-    return text;
-}
-public boolean isFound() {
-    return isFound;
-}
-public void setFound(boolean isFound) {
-    this.isFound = isFound;
-}
-public void setSearch_word(String search_word) {
-    this.search_word = search_word;
-}
-public void setText(String text) {
-    this.text = text;
-}
-public int getWord_counter() {
-    return word_counter;
-}
-public void setWord_counter(int word_counter) {
-    this.word_counter = word_counter;
-}
+    
+    // Getters and setters
+    public String getSearch_word() {
+        return search_word;
+    }
+    public String getText() {
+        return text;
+    }
+    public boolean isFound() {
+        return isFound;
+    }
+    public void setFound(boolean isFound) {
+        this.isFound = isFound;
+    }
+    public void setSearch_word(String search_word) {
+        this.search_word = search_word;
+    }
+    public void setText(String text_new) {
+        this.text = text_new;
+    }
+    public int getWord_counter() {
+        return word_counter;
+    }
+    public void setWord_counter(int word_counter) {
+        this.word_counter = word_counter;
+    }
+    public int getText_length() {
+        return text_length;
+    }
+    public String[] getWordsArray() {
+        return words;
+    }
+    public String getWordInArray(int i) {
+        return words[i];
+    }
 }
--- src/AppTest.java
+++ src/AppTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals; 
 import org.junit.Test;
 
@@ -52,27 +53,17 @@ public class AppTest {
     public void check_counter_function(){
         //arrange
         SearchWord searchWord = new SearchWord();
+        final String testWord = "Java8"; 
+        final String test_text = "Java8 makes Java more powerful";
         //act
+        searchWord.setSearch_word(testWord);
+        searchWord.setText(test_text);
         searchWord.word_counter();
-        searchWord.word_counter();
-        searchWord.word_counter();        
-        searchWord.word_counter();               
-        //assert
-        assertEquals(4, searchWord.getWord_counter());
-    }
-    
-    @Test
-    public void check_counter_call(){
-        //arrange
-        SearchWord searchWord = new SearchWord();
-        //act
-        searchWord.setSearch_word("Java8");   
-        searchWord.setText("Java8 makes Java more powerful");
-        searchWord.search();
+             
         //assert
         assertEquals(1, searchWord.getWord_counter());
     }
-
+    
     @Test
     public void check_counter_loop(){
         //arrange
@@ -84,4 +75,35 @@ public class AppTest {
         //assert
         assertEquals(2, searchWord.getWord_counter());
     }
+
+    @Test
+    public void check_array(){
+        //arrange
+        SearchWord searchWord = new SearchWord();
+        final String [] array_expected = {"Java8", "makes", "Java", "more", "powerful"};
+        
+        //act
+        searchWord.setText("Java8 makes Java more powerful");
+        searchWord.WordsCounter();
+        String[] array_received = searchWord.getWordsArray();
+        
+        //assert
+        assertArrayEquals(array_expected, array_received);
+    }
+
+    @Test
+    public void check_word_in_array(){
+        //arrange
+        SearchWord searchWord = new SearchWord();
+        final String word_expected = "makes";
+        
+        //act
+        searchWord.setText("Java8 makes Java more powerful Java8");
+        searchWord.WordsCounter();
+        String word_inArray = searchWord.getWordInArray(1);
+        
+        //assert
+        assertEquals(word_expected, word_inArray);
+    }
 }
+
